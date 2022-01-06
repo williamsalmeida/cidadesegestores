c6badc88a621e5e8adf76da36ddbf23e92f461a3
Williams Almeida <williannsbrown@gmail.com>
2022-01-06 10:31:16 -0300

    ajuste link video

--- src/components/EventBanner.js
+++ src/components/EventBanner.js
@@ -12,7 +12,7 @@ const EventBanner =()=>{
                     <h2 className="wow fadeInUp" data-wow-delay="0.8s"><span>Feira dos<br/></span>Municípios Alagoanos<br/><span>Congresso e Expo</span></h2>
                     <h6 className="wow fadeInUp mb-5" data-wow-delay="0.6s">Viva Alagoas nos seus detalhes</h6>
                     <a className="event_btn btn_hover wow fadeInLeft" data-wow-delay="0.9s" href="/#">Inscrições</a>
-                    <a className="event_btn event_btn_banner event_btn_two btn_hover wow fadeInRight" data-wow-delay="0.9s" href="https://www.youtube.com/watch?v=S719j6wChh0" target="_blank"><i className="arrow_triangle-right_alt2"></i>Conheça</a>
+                    <a className="event_btn event_btn_banner event_btn_two btn_hover wow fadeInRight" data-wow-delay="0.9s" href="https://www.youtube.com/watch?v=S719j6wChh0"><i className="arrow_triangle-right_alt2"></i>Conheça</a>
                 </div>
             </div>
         </section>
